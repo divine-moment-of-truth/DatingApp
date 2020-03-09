ece890b366668a2633568c25c61c5ee180cdce4a
unknown <shortzenhausen@gmail.com>
2020-03-09 21:00:08 +0000

    Addition of routing

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertifyService: AlertifyService) { }
+  constructor(public authService: AuthService, private alertifyService: AlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -23,6 +25,8 @@ export class NavComponent implements OnInit {
     }, error => {
       // console.log(error);
       this.alertifyService.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -30,15 +34,14 @@ export class NavComponent implements OnInit {
     // const token = localStorage.getItem('token');
     // The following code means it will return false if token is empty, or true if token has a value
     // return !!token;
-
     return this.authService.loggedIn();
 
   }
 
   logout() {
     localStorage.removeItem('token');
-    // console.log('Logged out!');
     this.alertifyService.message('Logged out!');
+    this.router.navigate(['/home']);
   }
 
 }
